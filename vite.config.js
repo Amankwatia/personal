a3b493248273e26841e361e3914bcd4d2ed78188
Amankwatia <brightamankwatia@gmail.com>
2026-07-01 00:25:34 +0000

    Fix Vite base path for Cloudflare Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  // GitHub Pages project site: https://amankwatia.github.io/personal/
-  // Change this to '/' when moving to a custom domain or user site.
-  base: '/personal/'
+  base: '/'
 })
